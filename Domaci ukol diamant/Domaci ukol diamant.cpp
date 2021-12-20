93a85e5b868dc11eedf02a466ee0d6e0bd259509
matěj <matj@DESKTOP-1HI5AKA>
2021-12-20 13:25:36 +0100

    Přidejte soubory projektu.

---
+++ Domaci ukol diamant/Domaci ukol diamant.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	int mezera = 9;
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < mezera; j++) {
+			printf(" ");
+		}
+		for (int k = i; k >= 1; k--) {
+			printf("%d", k);
+		}
+		for (int k = 2; k <= i; k++) {
+			printf("%d", k);
+		}
+		printf("\n");
+		mezera--;
+	}
+
+	mezera = 0;
+	for (int i = 9; i > 0; i--) {
+		for (int j = 0; j < mezera; j++) {
+			printf(" ");
+		}
+		for (int k = i; k >= 1; k--) {
+			printf("%d", k);
+		}
+		for (int k = 2; k <= i; k++) {
+			printf("%d", k);
+		}
+		printf("\n");
+		mezera++;
+	}
+
+}
